ffd033fbaae698650146fc347223e5ec3bc3631f
Sergio Arboleda Zuluaga <sergioarboledazuluaga@gmail.com>
2018-10-17 22:05:55 -0500

    No funciona el request a la api de whatsapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { HTTP } from '@ionic-native/http';
+/*import { SocialSharing } from '@ionic-native/social-sharing';*/
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    HTTP,
+    /*SocialSharing*/
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,118 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { HTTP } from '@ionic-native/http';
+/*import { SocialSharing } from '@ionic-native/social-sharing';*/
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  testRadioOpen: boolean;
+  testRadioResult;
+  countryCodes = {
+    "Colombia": '57',
+    "Argentina": '54',
+    "Bolivia": '591',
+    "Chile": '56'
+  };
+  countryCode = '57';
+  phoneNumber;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, private http: HTTP/*, private socialSharing: SocialSharing*/) {
 
   }
+  showRadio() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Country');
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Colombia',
+      value: 'Colombia',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Argentina',
+      value: 'Argentina',
+      checked: false
+    });
+    alert.addInput({
+      type: 'radio',
+      label: 'Bolivia',
+      value: 'Bolivia',
+      checked: false
+    });
+    alert.addInput({
+      type: 'radio',
+      label: 'Chile',
+      value: 'Chile',
+      checked: false
+    });
+
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'OK',
+      handler: data => {
+        this.testRadioOpen = false;
+        this.testRadioResult = data;
+        this.countryCode = this.countryCodes[this.testRadioResult];
+      }
+    });
+    alert.present().then(() => {
+      this.testRadioOpen = true;
+    });
+
+
+  }
+
+  showPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add number',
+      message: "Type the phone number to start the conversation",
+      inputs: [
+        {
+          name: 'number',
+          placeholder: this.countryCode + '3206765879'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log("Cancelled");
+          }
+        },
+        {
+          text: 'Send',
+          handler: data => {
+            this.phoneNumber = this.countryCode + data;
+            this.http.get('https://api.whatsapp.com/send?', {phone: this.phoneNumber}, {})
+              .then(data => {
+
+                console.log(data.status);
+                console.log(data.data); 
+                console.log(data.headers);
+
+              })
+              .catch(error => {
+
+                console.log(error.status);
+                console.log(error.error);
+                console.log(error.headers);
+
+              });
+            console.log('Sent');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
 
 }
